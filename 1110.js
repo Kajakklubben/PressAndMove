081d0806de781bd20b23a86d47f5f975fc842947
Jonas Jongejan <jonas@halfdanj.dk>
2012-09-25 20:41:34 +0200

    Inital commit

---
+++ 1110.js
@@ -0,0 +1,160 @@
+
+function eventPos(e) {
+	if(e.type.match(/^touch/)) {
+		e = e.originalEvent.changedTouches[0];
+	}
+	return {pageX: e.pageX, pageY: e.pageY};
+}
+
+var Map=function($container){
+	$container.css({
+		'z-index': 1,
+		overflow: 'hidden',
+		width: '740px',
+		height: '694px',
+		margin: '0px auto 0',
+		background: '#fff',
+		position: 'relative'
+	});
+
+	var $overlay = $container.children('img');
+	$overlay.css({
+		background: 'transparent',
+		position: 'relative'
+	});
+	
+	this.position = function() { return position };
+	this.update = function() { update(); };
+	this.info = function() { return {
+		padding_top: padding_top,
+		size: size,
+		tilesize: tilesize,
+		offset: offset,
+		container_size: container_size,
+		$map: $map,
+		map_size: map_size
+	}; };
+
+	var sign=function(x){return x>0?+1:x<0?-1:0;};
+	var pow=function(x,y){return Math.pow(Math.abs(x),y)*sign(x);}
+	var clamp=function(x,min,max){return Math.max(Math.min(x,max),min);}
+
+	var offset=$container.offset();
+
+	var padding_top=200;
+	var size=[14,48,25,33];
+	var tilesize=2048;
+	var visible=[];
+	var container_size=[$container.width(),$container.height()];
+	var scroll_delta=null;
+
+	var $map=$container.children('.map');
+
+	var map_size=[(size[1]+size[3])*tilesize,(size[0]+size[2])*tilesize];
+	$map.css({
+		width: map_size[0],
+		height: map_size[1],
+		position: 'absolute',
+		zIndex: -1
+	});
+
+	var position=[-(size[3]+0.03)*tilesize,-(size[0]-0.55)*tilesize];
+
+	$map.find('.ground').css({
+		top: size[0]*tilesize,
+		height: size[2]*tilesize,
+		position: 'absolute',
+		width: '100%',
+		zIndex: -1,
+		background: '#000'
+	});
+
+	var centre=[-1,0];
+
+	var update=function(){
+		$map.css({
+			left:position[0],
+			top:position[1]
+		});
+
+		var centre_last=centre;
+		centre=[Math.floor(-position[0]/tilesize),Math.floor(-position[1]/tilesize)];
+
+		tile_name=function(x,y){
+			x-=size[3];
+			y-=size[0];
+			return (y>=0?(y+1)+'s':-y+'n')+(x>=0?(x+1)+'e':-x+'w');
+		};
+
+		if(centre[0]!=centre_last[0]||centre[1]!=centre_last[1]){
+			var $remove=$map.children().not('.ground').not('#stickfigure');
+
+			for(var y=-1;y<=+1;y++)
+			for(var x=-1;x<=+1;x++){
+				var name=tile_name(centre[0]+x,centre[1]+y);
+				var tile=$map.find('.tile'+name);
+				if(tile.length)
+					$remove=$remove.not(tile);
+				else {
+					$image=$('<img class="tile'+name+'" src="imgs/'+name+'.png" style="top:'+((centre[1]+y)*tilesize)+'px;left:'+((centre[0]+x)*tilesize)+'px; z-index: -1; position: absolute;;" style="display:none" />');
+
+					$image.load(function(){$(this).show()}).error(function(){$(this).remove();});
+					$map.append($image);
+					if($("#canvascontainer #" + name).length == 0) {
+						var colImg = $(new Image());
+						colImg.attr('src', 'imgs/'+name+'C.png')
+						colImg.load(function(nm) { return function() {
+							canvas = "<canvas id='"+nm+"' width='2048' height='2048'></canvas>";
+							
+							$("#canvascontainer").append(canvas);
+							var context = document.getElementById(nm).getContext('2d');
+							context.drawImage(this, 0, 0);
+						}}(name)).error(function (nm) { return function() {
+							if(!this.errorset) {
+								$(this).attr('src', 'imgs/'+nm+'.png');
+								this.errorset = true;
+							}
+							
+						}}(name));
+					}
+					
+					$("#canvascontainer").hide();
+					
+				}
+			}
+
+			$remove.remove();
+		}
+	}
+
+	update();
+
+	function drag(e){
+		if(scroll_delta){
+			var pos = eventPos(e);
+			position[0]=Math.round(clamp(pos.pageX+scroll_delta[0],-(size[1]+size[3])*tilesize+container_size[0],0));
+			position[1]=Math.round(clamp(pos.pageY+scroll_delta[1],-(size[0]+size[2])*tilesize+container_size[1],0));
+			update();
+		}
+	}
+
+	/*$container
+		.on('mousedown touchstart', function(e){
+			if(e.button && e.button >= 2){
+				return;
+			}
+			var pos = eventPos(e);
+			scroll_delta=[position[0]-pos.pageX,position[1]-pos.pageY];
+			$(document).on(e.type == 'mousedown' ? 'mousemove' : 'touchmove', drag);
+			e.preventDefault();
+		})
+	;
+	$(document)
+		.on('mouseup touchend', function(e){
+			$(document).off('mousemove touchmove', drag)
+			scroll_delta=null;
+		})
+	;*/
+};
+
+/* 50:72:6f:50:75:6b:65:20:69:73:20:61:77:65:73:6f:6d:65 */
---
+++ game.js
@@ -0,0 +1,353 @@
+var upPressed = false;
+var downPressed = false;
+var leftPressed = false;
+var rightPressed = false;
+var debugSpeed = false;
+var balloon = false;
+var upPressedNow = false;
+var isGrounded = false;
+
+function pad(number, length) {
+   
+    var str = '' + number;
+    while (str.length < length) {
+        str = '0' + str;
+    }
+   
+    return str;
+
+}
+function cache(event, b) {
+	var triggered = false;
+	
+	switch(event.which) {
+		case 37:
+			leftPressed = b;
+			triggered = true;
+			 break;
+		case 38:
+			if(!upPressed)
+				upPressedNow = true;
+			upPressed = b;
+			triggered = true;
+			
+			break;
+		case 39:
+			rightPressed = b;
+			triggered = true;
+			break;
+		case 40:
+			downPressed = b;
+			triggered = true;
+			break;
+		case 70:
+			debugSpeed = b;
+			triggered = true;
+			break;
+		case 32:
+			balloon = b;
+			triggered = true;
+			break;
+		
+	}
+	return !triggered;
+};
+
+document.onkeypress = function(event) {
+	if(event.which == 106) {
+		var s = prompt("Where do you wanna go?");
+
+		var grp = (/(\d*)(n|s)(\d*)(w|e)/).exec(s);
+		var x = 0;
+		var y = 0;
+		
+		if(grp[4] == "w")
+			x = -parseInt(grp[3]);
+		else if(grp[4] == "e")
+			x = parseInt(grp[3]);
+
+		if(grp[2] == "n")
+			y = -parseInt(grp[1]);
+		else if(grp[2] == "s")
+			y = parseInt(grp[1])-1;
+			
+		player.x = x*2048;
+		player.y = y*2048;
+	}
+};
+
+document.onkeydown = function(event) { return cache(event, true);  };
+document.onkeyup = function(event) { return cache(event, false); };
+
+function Player() {
+	var $map = $(".map");
+	$map.append('<img src="man.png" style="position: relative; z-index: 20" id="stickfigure">');
+	this.player = $("#stickfigure", $map);
+	
+	this.x = 0;
+	this.y = 0;
+	
+	this.vx = 0;
+	this.vy = 0;
+	
+	var lastPressed = "right";
+	
+	var frame = 0;
+	var animation = "fall";
+	
+	var cnt = 0;
+	
+	this.update = function () {
+		var factor = 1;
+		if(debugSpeed) {
+			factor = 10;
+		}
+		if(!isGrounded)
+			factor = 0.5;
+			
+		if(leftPressed) {
+			
+			player.vx = -8*factor;
+			lastPressed = "left";
+		}
+		else if(rightPressed) {
+			player.vx = 8*factor;
+			lastPressed = "right";
+		}
+		else
+			player.vx = 0;
+			
+		if(upPressedNow) {
+			if(isGrounded)
+				player.vy = -10;
+			upPressedNow = false;
+		}
+		if(balloon)
+		{
+			if(player.vy>-6)
+				player.vy += -2;
+				
+			
+		}
+		
+		cnt=(cnt+1)%6;
+		if(cnt == 0)
+			this.animate();
+		
+		if(lastPressed == "right") {
+			this.player.addClass("flip-horizontal");
+		}
+		else {
+			this.player.removeClass("flip-horizontal");
+		}
+	}
+	
+	this.animate = function() {
+		if(player.vy > 5) {
+			animation = "fall"
+			frame = 0;
+		}
+		else if(player.vy < 0) {
+			frame = 0;
+			animation = "air"
+		}
+		else if(Math.abs(player.vx) > 1) {
+			if(animation == "run") {
+				frame = (frame+1)%2;
+			}
+			else {
+				frame = 0;
+			}
+				
+			animation = "run"
+		}
+		else {
+			if(animation == "idle") {
+				frame = (frame+1)%2;
+			}
+			else {
+				frame = 0;
+			}
+				
+			animation = "idle"
+		}
+		
+		var ani = "player\\man_" + animation +  "_" + pad(1+frame, 2) + ".png";
+		
+		this.player.attr("src", ani);
+	}
+}
+var camx;
+function update() {
+	player.update();
+	
+	updatePhysics();
+	
+	oldPlayerX = player.x;
+	
+	player.x += player.vx;
+	player.y += player.vy;
+	
+	var x = initMapPos[0] - player.x;
+	var y = initMapPos[1] - player.y;
+	
+	var dx = 0;
+	
+	map.position()[0] = x+dx;
+	map.position()[1] = y;
+	
+	map.update();
+	
+	//player.player.offset({left: 650, top: 400});
+	player.player.offset({left: 650+dx, top: 400});
+}
+
+var groundedFrames;
+function updatePhysics()
+{
+	if(groundedFrames==0)
+		isGrounded = false;
+	else
+		groundedFrames--;
+		
+	var playerWidth = 20;
+	var playerHeight = 37;
+	player.vy += 1;
+	
+	if(player.vy > 20) { //max fall speed
+		vy = 20;
+	}
+		
+
+	var dirX =player.vx>0?1:-1;
+	var dirY =player.vy>0?1:-1;
+	var centerHorDist = PlayerRaytrace(dirX*playerWidth/2,playerHeight/5,dirX,0,Math.abs(player.vx));
+	var stopHorizontal = false;
+	if(centerHorDist!=-1)
+	{
+	
+		player.x = player.x+dirX*(centerHorDist+playerWidth/2)-dirX*playerWidth/2;
+		
+		player.vx = 0;
+		stopHorizontal = true;
+		
+	}
+	
+	var centerVerDist = PlayerRaytrace(0,dirY*playerHeight/2,0,dirY,Math.abs(player.vy));
+	if(centerVerDist != -1) {
+		
+		groundedFrames = 3; // we are grounded for the next 3 frames
+		isGrounded = true; 
+		player.vy = 0;
+		
+		var dist = PlayerRaytrace(-1,dirY*playerHeight/2+Math.abs(player.vy),0,-dirY,playerHeight,true);
+		if(dist != -1)
+		{
+			player.y = player.y - (dirY*dist)+1 +dirY*centerVerDist;
+			//player.vx *=0.9;
+			
+		}
+		else
+		player.y = player.y+dirY*(centerVerDist);
+
+	}
+
+}
+function PlayerRaytrace(xoffset,yoffset,dx,dy,dist,flip) {
+	if(flip == undefined)
+		flip = false;
+	var x = player.player.position().left+player.player.width()/2+xoffset;
+	var y = player.player.position().top + player.player.height()/2+yoffset;
+	
+	var maxraytrace = 6.0; //lower this to gain performance. 10 might be too small
+	
+	var step = Math.ceil(dist/maxraytrace);
+	
+	var ground = false;
+	for(i = 0;i<dist;)
+	{
+		ground = groundAtPixel(x+i*dx, y+i*dy);
+		if((!flip && ground) || (flip && !ground))
+			return i;
+		//we always want to trace the first 3 pixels due to better walking
+		if(i<3)
+			i++;
+		else
+			i+=step;
+			
+	}
+	return -1;
+
+}
+
+function PlayerFeetCollision(dx,dy) {
+	var leftX = player.player.position().left+dx;
+	var rightX = player.player.position().left + player.player.width()+dx;
+	var y = player.player.position().top + player.player.height()+dy;
+	
+	if(groundAtPixel(leftX, y) || groundAtPixel(rightX, y))
+		return true;
+	
+	return false;
+}
+
+function PlayerBodyCollision(dx,dy) {
+	var leftX = player.player.position().left+dx;
+	var rightX = player.player.position().left + player.player.width()+dx;
+	var y = player.player.position().top + player.player.height()/2+dy;
+	
+	if(groundAtPixel(leftX, y) || groundAtPixel(rightX, y))
+		return true;
+	
+	return false;
+}
+
+function groundAtPixel(x, y) {
+	var img = getImageForPixel(x, y)[0];
+
+	if(img != undefined)  {
+		var splitted = img.src.substring(0, img.src.length - 4).split("/");
+		
+		var name = splitted[splitted.length-1];
+
+		var localX = x - $(img).position().left;
+		var localY = y - $(img).position().top;
+		
+		if(groundAtImagePixel(name, localX, localY))
+			return true;
+	}
+	return false;
+}
+	
+function groundAtImagePixel(name, x, y) {
+	if(document.getElementById(name) == null)
+		return false; 
+	var context = document.getElementById(name).getContext('2d');
+	data = context.getImageData(x, y, 1, 1).data;
+	return data[0] < 50;
+}
+	
+function getImageForPixel(x, y) {
+	return $(".map img").not("#stickfigure").filter(function(index) {
+		var $this = $(this);
+		var lowerX = $this.position().left;
+		var upperX = $this.position().left +  $this.width();
+
+		var lowerY = $this.position().top;
+		var upperY = $this.position().top +  $this.height();
+		
+		if(lowerX < x && x < upperX && lowerY < y && y < upperY)
+			return true;
+		return false;
+	});
+}
+
+var player;
+var map;
+var initMapPos ;
+$(function() {
+	map=new Map($('#comic'));
+	initMapPos = [Math.floor(map.position()[0]), Math.floor(map.position()[1])];
+	player = new Player();
+	window.setInterval(update,30);
+});
